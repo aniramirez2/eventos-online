88c769b48bb83f20d62b0d0afb1a46273f17612e
Cecília Assis <cy_assis@hotmail.com>
2020-05-02 03:03:35 -0300

    Join Django + React

--- README.md
+++
@@ -1,5 +0,0 @@
-# ongs-app
-
-Use npm start to run the app
-
-Deplyoed in Netlify https://be-the-hero-ongs.netlify.com
---
+++ wrapper/frontend/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ]
+            }
+        ]
+    }
+};
