0a74f7edbaeff5e6f2210c09a41989b1836619d7
Cecília Assis <cy_assis@hotmail.com>
2020-05-03 16:23:40 -0300

    [api] Enable user logout

--- makelinks/accounts/urls.py
+++ makelinks/accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
-    # path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('api/occupation_areas', OccupationAreaAPI.as_view()),
     path('api/interests', InterestAPI.as_view()),
 ]
