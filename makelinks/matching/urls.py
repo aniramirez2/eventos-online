9c60b18175737db03518a77fa5efdd9825dc1721
Cecília Assis <cy_assis@hotmail.com>
2020-05-04 15:14:09 -0300

    [api] Change networking endpoints

--- makelinks/matching/urls.py
+++ makelinks/matching/urls.py
@@ -1,8 +1,9 @@
-from matching.api import MatchingViewSet, MatchRecommendationAPI
+from matching.api import MatchTriggerViewSet, MatchRecommendationAPI, MatchAPI
 from django.urls import path, include
 
 
 urlpatterns = [
-    path('api/match', MatchingViewSet.as_view({'post': 'match'})),
-    path('api/match_recommendations', MatchRecommendationAPI.as_view()),
+    path('api/network/start', MatchTriggerViewSet.as_view({'post': 'match'})),
+    path('api/network/match', MatchAPI.as_view()),
+    path('api/network/recommendation', MatchRecommendationAPI.as_view()),
 ]
